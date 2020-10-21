989f8b14796c804ddaed2aba82955e649d6e0739
icampill <ilancampillo@gmail.com>
2020-10-21 15:22:34 +0200

    first commit

---
+++ server/database/db.js
@@ -0,0 +1,35 @@
+/*
+const Sequelize = require('sequelize')
+const db = {}
+const sequelize = new Sequelize('matcha', 'root', 'root', {
+  port: 3307,
+  dialect: 'mysql',
+  operatorsAliases: false,
+
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+})
+
+db.sequelize = sequelize
+db.Sequelize = Sequelize
+
+module.exports = db
+*/
+
+var mysql      = require('mysql')
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  port     : 3307,
+  user     : 'root',
+  password : 'root',
+  database : 'matcha'
+})
+
+connection.connect()
+
+module.exports = connection
